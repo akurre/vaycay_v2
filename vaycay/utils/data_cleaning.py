e1c76e5a75890e765caff87c45609038fea93453
Ashlen Kurre <ashlen.kurre@netlight.com>
2024-04-16 09:49:59 +0200

    data cleaning scripts

--- vaycay/utils/data_cleaning.py
+++ vaycay/utils/data_cleaning.py
@@ -1,74 +1,72 @@
-from heapq import merge
-import numpy as np
 import pandas as pd
-from sqlalchemy import Column, Integer, Float, String, Date, null
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+import time
 import json
 
-
-# Base = declarative_base()
-
-# class cities_and_weather(Base):
-#     __tablename__ = 'all_city_weather_data'
-#     city = Column(String, primary_key=True, nullable=True)
-#     country = Column(String, nullable=True)
-#     date = Column(Date, primary_key=True, nullable=True)
-#     data_type = Column(String, primary_key=True, nullable=True)
-#     lat = Column(Float, nullable=True)
-#     long = Column(Float, nullable=True)
-#     population = Column(Float, nullable=True)
-#     name = Column(String, nullable=True)
-#     avg_temp_celcius = Column(Float, nullable=True)
-#
-#     class Config:
-#         orm_mode = True
+# constants
+pd.set_option('display.max_columns', None)
+all_weather_data_csv = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/AVERAGED_weather_station_data_ALL.csv'
+world_cities_csv = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/worldcities.csv'
+country_to_filter = 'Italy'
+population_limit = 10000
 
 
 if __name__ == "__main__":
-    # engine = create_engine('postgresql://postgres:iwantsun@localhost:5432/postgres')
-    # Base.metadata.create_all(engine)
-
     # Read in weather data
-    print("Reading in weather data from CSV - DB engine created. Please wait. ")
-    file_name = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/AVERAGED_weather_station_data_ALL.csv'
-    df = pd.read_csv(file_name, usecols = ['id','date', 'data_type', 'lat', 'long', 'name', 'AVG'], float_precision=None)
+    print("Reading in weather data from CSV. Please wait. ")
+    start_time = time.time()
+    df = pd.read_csv(
+        all_weather_data_csv,
+        usecols=['id', 'date', 'data_type', 'lat', 'long', 'name', 'AVG'],
+        float_precision=None
+    )
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
+    # rename rows and refactor DDMM dates
+    start_time = time.time()
     df = df.rename(columns={'AVG': 'avg_temp_celcius'})
     df['date'] = ((df['date'].astype(str).str.zfill(4)) + '2020')
     df['date'] = pd.to_datetime(df['date'], format='%m%d%Y')
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
-    # Drop unneeded rows 
+    # Drop unneeded rows
+    print('Dropping unneeded weather data rows')
     df = df[df['data_type'].isin(['TMIN', 'TMAX', 'TAVG', 'PRCP'])]
 
-    # # Delete cities with only precip
-    # grouped = df.groupby('name')
-    # for name, group in grouped:
-    #     unique = df['data_type'].unique()
-    #     if 'T' not in ''.join(unique):
-    #         df.drop(grouped.get_group(name).index)
-    #         print("PRCP dropped!")
+    # Delete cities with only precip
+    start_time = time.time()
+    print('Deleting cities with only precipitation data.')
+    grouped = df.groupby('name')
+    for name, group in grouped:
+        unique = df['data_type'].unique()
+        if 'T' not in ''.join(unique):
+            df.drop(grouped.get_group(name).index)
+            print(f"{name} only has PRCP data - dropped!")
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
     # Read in city/population data
-    pop_limit = 10000
-    print(f'Reading in population dataframe - purging cities with population < {pop_limit}')
-    file_name = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/worldcities.csv'
-    df2 = pd.read_csv(file_name)
+    start_time = time.time()
+    print(f'Reading in population dataframe - purging cities with population < {population_limit}')
+    df2 = pd.read_csv(world_cities_csv)
     df2 = df2[['city', 'country', 'lat', 'lng', 'population']]
     df2 = df2.rename(columns={'lng': 'long'})
-    # Filter for Italy only below
-    df2 = df2[df2['country'] == 'Italy']
-    pop = df2['population'].unique().tolist()
-    df2.drop(df2[df2.population < pop_limit].index, inplace=True)
-    
+    df2 = df2[(df2['population'] >= population_limit)]
+    # Filter for country below
+    if country_to_filter:
+        df2 = df2[(df2['country'] == country_to_filter)]
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
     # merge dataframes on nearest 0.5 lat/long, inner join
+    start_time = time.time()
     print('Joining dataframes')
     df2['lt'] = df2['lat'].mul(2).round().div(2)
     df2['lng'] = df2['long'].mul(2).round().div(2)
     df['lt'] = df['lat'].mul(2).round().div(2)
     df['lng'] = df['long'].mul(2).round().div(2)
-    merged_df = df2.merge(df, how = 'inner', on = ['lt', 'lng'])
+    merged_df = df2.merge(df, how='inner', on=['lt', 'lng'])
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
     # get duplicates/same city
+    start_time = time.time()
     print('Removing duplicate city entries')
     minimized = merged_df.groupby('city')
     dupes_to_delete = []
@@ -80,22 +78,67 @@ if __name__ == "__main__":
                 dupes_to_delete.append(x)
 
     # purge duplicates
-    print('Cleaning up')
+    print('Purging duplicates')
     merged_df = merged_df[~merged_df['date'].isin(dupes_to_delete)]
 
-    # clean up dupe columns
+    # clean up duplicate columns
     df = merged_df.rename(columns={'lat_x': 'lat', 'long_x': 'long'})
     df = df.drop(columns=['lat_y', 'long_y', 'lt', 'lng', 'id'])
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
     # pivot tables to give temp/prcp their own columns, round temp columns
+    start_time = time.time()
     print('Pivoting dataframe')
     df = df.pivot_table(index=['city', 'country', 'lat', 'long', 'population', 'date', 'name'], columns='data_type', values='avg_temp_celcius').reset_index()
     temp_cols = ['TMAX', 'TAVG', 'TMIN']
     for column in temp_cols:
         df[column] = df[column].div(10).round(2)
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
-    # save
+    # save just in case something goes wrong later
     print('Saving...this could take awhile.')
-    df.to_csv('/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.csv')
+    df.to_csv(f'/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/16April2024/minimized_weather_population_station_data_cleaned_{population_limit}population_{country_to_filter}.csv')
 
+    # further removing stations with least data
+    start_time = time.time()
+    print('Removing duplicate city entries')
+    minimized = df.groupby('city')
+    dupes_to_delete = []
+    for city, group_df in minimized:
+        unique_list = group_df['name'].unique().tolist()
+        if len(unique_list) > 1:
+            sub_df = group_df.groupby('name')
+            weather_data_nulls = {}
+            for name, sub_group_df in sub_df:
+                sums = sub_group_df[['PRCP', 'TAVG', 'TMAX', 'TMIN']].isna().sum().sum()
+                weather_data_nulls[name] = sums
+            stations_list = [*weather_data_nulls]
+            minimum_nulls_station = min(weather_data_nulls, key=weather_data_nulls.get)
+            stations_list.remove(minimum_nulls_station)
+            for station in stations_list:
+                dupes_to_delete.append(station)
 
+    # purge duplicates
+    print('Cleaning up')
+    df = df[~df['name'].isin(dupes_to_delete)]
+    df = df.loc[:, ~df.columns.str.match('Unnamed')]
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
+    # fill in averages if TAVG is not available, but TMAX and TMIN are available
+    print('Filling in TAVG if not available')
+    df['TAVG'] = df['TAVG'].fillna(df[['TMAX', 'TMIN']].mean(axis=1))
+
+    # save csv
+    print('Saving...this could take awhile.')
+    df.to_csv(
+        f'/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_{pop_limit/1000}k_population_{country}.csv',
+        index=False)
+
+    # transform csv to json
+    print('transforming csv to json')
+    file_name = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.csv'
+    df = pd.read_csv(file_name, nrows=100000)
+    # print(df)
+    data = df.to_json(orient="records", force_ascii=False, indent=4)
+    with open('../weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.py', 'w') as f:
+        json.dump(data, f)
--- vaycay/utils/data_cleaning_2.py
+++ vaycay/utils/data_cleaning_2.py
@@ -1,76 +1,125 @@
-from heapq import merge
-import numpy as np
-import pandas as pd
-from sqlalchemy import Column, Integer, Float, String, Date, null
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 import json
-
+import pandas as pd
+import geopandas as gpd
+import time
 
 if __name__ == "__main__":
-    # isolate cities and countries covered
-    file_name = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.csv'
-    df = pd.read_csv(file_name)
+    pd.set_option('display.max_columns', None)
+    start_time = time.time()
 
-    # # create json file of countries covered
-    # df_min = df[['city', 'country']]
-    # df_grp = df_min.groupby('country')
-    # cities_dict = {}
-    # for country, cities in df_grp:
-    #     print('COUNTRY: ', country, '\n CITIES: ', cities['city'].unique())
-    #     cities_dict[country] = cities['city'].unique().tolist()
-    # with open('data/cities_countries_covered_population_30k.json', 'w') as fp:
-    #     json.dump(cities_dict, fp)
+    weather_file = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/AVERAGED_weather_station_data_ALL.csv'
+    city_file = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/worldcities.csv'
+    population_filter = 10000
+    json_rows_limit = 100000
+
+    # Read in weather data  (past time taken: 22.48 seconds)
+    print("Reading in weather data from CSV. Please wait.")
+    df = pd.read_csv(weather_file, nrows=10000)
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
+    # Rename and reformat columns (past time taken: 21.13 seconds)
+    start_time = time.time()
+    print('Renaming and reformatting columns to datetime.')
+    df.rename(columns={'AVG': 'avg_temp_celcius'}, inplace=True)
+    df['date'] = pd.to_datetime(df['date'].astype(str).str.zfill(4) + '2020', format='%m%d%Y')
+    df = df[df['data_type'].isin(['TMIN', 'TMAX', 'TAVG', 'PRCP'])]
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
+    # Filter out weather stations with only precipitation data (past time taken: 12.17 seconds)
+    start_time = time.time()
+    print('Filtering out weather stations with only precipitation data.')
+    grouped = df.groupby('name')
+    for name, group in grouped:
+        if 'T' not in group['data_type'].unique():
+            df.drop(group.index, inplace=True)
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+
+    # Read in city data, focusing on Italian cities with a significant population (past time taken: 0.06 seconds)
+    print(f'Reading in population dataframe - purging cities with population < {population_filter}')
+    world_cities_csv = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/uncleaned_data/worldcities.csv'
+    df2 = pd.read_csv(world_cities_csv)
+    df2 = df2[['city', 'country', 'lat', 'lng', 'population']]
+    df2 = df2.rename(columns={'lng': 'long'})
+    # Filter for country below
+    country = 'Italy'
+    df2 = df2[(df2['country'] == country) & (df2['population'] >= population_filter)]
+
+    # Convert dataframes to GeoDataFrames (past time taken: 3.10 seconds)
+    start_time = time.time()
+    print('Convert dataframes to GeoDataFrames.')
+    gdf_stations = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.long, df.lat))
+    gdf_cities = gpd.GeoDataFrame(df2, geometry=gpd.points_from_xy(df2.long, df2.lat))
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+    print(df.head())
+    print(df2.head())
+    exit()
+
+    # Spatial join - find the nearest weather station for each city (past time taken: 73 minutes with 10k pop)
+    start_time = time.time()
+    print('Finding the nearest weather station for each city.')
+    nearest_station_ids = []
+    for index, city in gdf_cities.iterrows(): # Iterate over each row in the cities GeoDataFrame
+        # Find the nearest weather station's index
+        nearest_station_id = gdf_stations.distance(city.geometry).idxmin()
+        nearest_station_ids.append(nearest_station_id)
+        if index % 10 == 0:
+            print(f"Record {index}: Processing {city['city']} - Nearest Station ID: {nearest_station_id}")
+    # Assign the list of nearest station IDs back to the DataFrame
+    df2['nearest_station_id'] = nearest_station_ids
+    merged_df = df2.join(gdf_stations.loc[df2['nearest_station_id']].set_index(df2.index), rsuffix='_station')
+    print(f'Time taken: {time.time() - start_time:.2f} seconds')
+
+    # save in case of issues
+    print('Saving intermediate step...')
+    output_file_int = f'/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/INTERMEDIATE_{population_filter / 1000}k_population_Italy_aigen.csv'
+    # merged_df.to_csv(output_file_int, index=False)
+    # print(merged_df.head())
 
 
-    # further removing stations with least data
-    print('Removing duplicate city entries')
-    minimized = df.groupby('city')
-    dupes_to_delete = []
-    for city, group_df in minimized:
-        unique_list = group_df['name'].unique().tolist()
-        if len(unique_list) > 1:
-            sub_df = group_df.groupby('name')
-            weather_data_nulls = {}
-            for name, sub_group_df in sub_df:
-                sums = sub_group_df[['PRCP', 'TAVG', 'TMAX', 'TMIN']].isna().sum().sum()
-                weather_data_nulls[name] = sums
-            stations_list = [*weather_data_nulls]
-            minimum_nulls_station = min(weather_data_nulls, key=weather_data_nulls.get)
-            stations_list.remove(minimum_nulls_station)
-            for station in stations_list:
-                dupes_to_delete.append(station)
+    # read in csv again in case of exit
+    merged_df = pd.read_csv(output_file_int)
+    sample_df = merged_df.loc[merged_df['city'] == 'Abbiategrasso']
+    print(sample_df.head(10))
 
-    # purge duplicates
-    print('Cleaning up')
-    df = df[~df['name'].isin(dupes_to_delete)]
-    df = df.loc[:, ~df.columns.str.match('Unnamed')]
+    # Pivot table for clean organization (past time taken:  seconds)
+    start_time = time.time()
+    print('Pivoting table.')
+    final_df = merged_df.pivot_table(index=['city', 'country', 'lat', 'long', 'population', 'date'],
+                                     columns='data_type', values='avg_temp_celcius').reset_index()
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+    print(final_df.head(10))
+    exit()
 
-    # fill in averages if not available
-    df['TAVG'] = df['TAVG'].fillna(df[['TMAX', 'TMIN']].mean(axis=1))
-    print(df)
+    # Calculate average temperature if TAVG is missing (past time taken:  seconds)
+    start_time = time.time()
+    print('Calculating missing TAVG.')
+    final_df['TAVG'] = final_df.apply(
+        lambda row: (row['TMAX'] + row['TMIN']) / 2 if pd.isna(row['TAVG']) else row['TAVG'], axis=1)
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
 
-    # save
-    print('Saving...this could take awhile.')
-    df.to_csv(
-        '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.csv',
-        index=False)
+    # Convert temperatures to Celsius and round them (past time taken:  seconds)
+    start_time = time.time()
+    print('Converting to Celsius and rounding.')
+    temp_cols = ['TMAX', 'TAVG', 'TMIN']
+    for column in temp_cols:
+        final_df[column] = final_df[column].div(10).round(2)
+    print(f'Time taken: {time.time() - start_time:.2f} seconds. \n\n')
+    print(final_df.head(60))
 
-    '''
-    # migrate to PostgreSQL
-    file_name = 'data/minimized_weather_population_station_data_cleaned_10k_population.csv'
-    df = pd.read_csv(file_name)
-    print("Transformations of dataframe complete. Now importing into PostgreSQL db. Preview of data: \n", df.head(15))
-    df.to_sql(con=engine, index_label='index', name=cities_and_weather.__tablename__, if_exists='replace')
-    print("Migration to PostgreSQL DB complete. ")
-    '''
 
-    # transform csv to json
-    print('transforming csv to json')
-    file_name = '/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.csv'
-    df = pd.read_csv(file_name, nrows=100000)
-    # print(df)
+    # Save the cleaned data (past time taken:  seconds)
+    output_file = f'/Users/ashlenlaurakurre/Documents/GitHub/vaycay_v2/vaycay/weather_data/minimized_weather_population_station_data_cleaned_{population_filter/1000}k_population_Italy_aigen.csv'
+    start_time = time.time()
+    print(f'Saving csv...this could take awhile.')
+    final_df.to_csv(output_file, index=False)
+    print(f"Data saved successfully! \n Time taken: {time.time() - start_time:.2f} seconds. \n\n")
+    exit()
+
+    # Transform csv to json (past time taken:  seconds)
+    start_time = time.time()
+    print(f'Transforming csv to json (.py) with limit of {json_rows_limit} rows.')
+    df = pd.read_csv(output_file, nrows=json_rows_limit)
     data = df.to_json(orient="records", force_ascii=False, indent=4)
-    with open('../weather_data/minimized_weather_population_station_data_cleaned_10k_population_Italy.py', 'w') as f:
+    with open(f'{output_file[:-4]}.py', 'w') as f:
         json.dump(data, f)
-    # print(data)
+    print(f'JSON file saved successfully! \n Time taken: {time.time() - start_time:.2f} seconds. \n\n')
