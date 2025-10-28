d834f75c3d71ee7d8dae760d32a1fbe06267233d
Ashlen Kurre <ashlen.kurre@netlight.com>
2025-10-28 11:48:15 +0100

    update to vite

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(), // This enables path aliases from tsconfig.json
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: true,
+  },
+  envPrefix: 'REACT_APP_', // Support existing REACT_APP_ env variables
+});
