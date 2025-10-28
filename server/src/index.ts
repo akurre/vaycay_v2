eeb19124a02ba89b2088f39f2a5c7757e8327b41
Ashlen Kurre <ashlen.kurre@netlight.com>
2025-10-28 19:38:22 +0100

    client fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { config } from 'dotenv';
 import schema from './schema';
-import { context } from './context';
+import context from './context';
 
 // Load environment variables
 config();
